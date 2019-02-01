a1b633c9c47a8777bdc15779820716b63c305044
summercake@qq.com <summercake@qq.com>
2019-02-01 00:48:51 -0600

    edit user page 3

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,22 +10,25 @@ use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 class User extends Authenticatable implements MustVerifyEmail
 {
     use Notifiable, MustVerifyEmailTrait;
-
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
+        'avatar',
+        'introduction',
     ];
-
     /**
      * The attributes that should be hidden for arrays.
      *
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
     ];
 }
