1298adae81ed1fd4b8f46ca912bcff9cbd185585
summercake@qq.com <summercake@qq.com>
2019-02-01 06:22:39 -0600

    add reply

--- routes/web.php
+++ routes/web.php
@@ -49,3 +49,5 @@ Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
 // upload_image
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
+
+Route::resource('replies', 'RepliesController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
