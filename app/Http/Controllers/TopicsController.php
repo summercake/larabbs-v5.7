552ecc2edcf3fccbba5e2d3f502d7d635186c3af
summercake@qq.com <summercake@qq.com>
2019-02-02 01:51:26 -0600

    add command

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
 use Auth;
-
+use App\Models\User;
 class TopicsController extends Controller
 {
     public function __construct()
@@ -17,11 +17,12 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-    public function index(Request $request, Topic $topic)
+    public function index(Request $request, Topic $topic, User $user)
     {
         $topics = $topic->withOrder($request->order)->paginate(20);
-
-        return view('topics.index', compact('topics'));
+        $active_users = $user->getActiveUsers();
+        dd($active_users);
+        return view('topics.index', compact('topics', 'active_users'));
     }
 
     public function show(Request $request, Topic $topic)
